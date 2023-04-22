1be1e23821049b44c5e7affdf7fd722c72dc011c
Amit <amitsamant@gmail.com>
2023-04-22 16:02:09 +0530

    twitter streaming

---
+++ twitter-to-kafka-service/src/main/java/com/twitter/streaming/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,40 @@
+package com.twitter.streaming.demo.twitter.to.kafka.service;
+
+import com.twitter.streaming.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.twitter.streaming.demo.twitter.to.kafka.service.runner.StreamRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.twitter.streaming.demo")
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, StreamRunner streamRunner) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("App starts...");
+        System.out.println("hiii");
+        logger.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[]{})));
+        streamRunner.start();
+    }
+}
